1245ea2a2bfb710499bd2b23e2cc9e1a6ebdec82
Marvin <44047272+marvinxu99@users.noreply.github.com>
2019-10-02 20:08:31 -0700

    before firbase

---
+++ src/components/form-input/form-input.component.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import './form-input.styles.scss';
+
+const FormInput = ({ handleChange, label, name, type, value, required }) => (
+  <div className='group1'>
+    <input className='form-input' 
+      onChange={handleChange} 
+      name={name} 
+      type={type}
+      value={value}
+      required={required}
+    />
+    {label ? (
+      <label 
+        className={`${
+            value.length ? 'shrink' : ''
+        } form-input-label`}
+      >
+        {label}
+      </label>)
+      : null
+    }
+  </div>
+)
+
+export default FormInput;
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -13,6 +13,9 @@ const Header = () => (
       <Link className='option' to='/shop'>
         SHOP
       </Link>
+      <Link className='option' to='/signin'>
+        SIGN IN
+      </Link>
       <Link className='option' to='/shop'>
         CONTACT
       </Link>
