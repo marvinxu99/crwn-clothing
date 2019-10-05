a21b1cfa803950f3b04d437acfca616710fc858d
Marvin <44047272+marvinxu99@users.noreply.github.com>
2019-10-04 19:34:22 -0700

    added sign up page

---
+++ src/components/sign-up/sign-up.component.jsx
@@ -0,0 +1,104 @@
+import React from 'react';
+
+import './sign-up.styles.scss';
+
+import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
+
+import { auth, createUserProfileDocument } from '../../firebase/firebase.utils';
+
+
+class SignUp extends React.Component {
+    constructor() {
+      super();
+  
+      this.state = {
+        displayName: '',
+        email: '',
+        password: '',
+        confirmPassword: ''
+      }
+    }
+  
+    handleSubmit = async event => {
+      event.preventDefault();
+
+      const {displayName, email, password, confirmPassword} = this.state;
+
+      if (password !== confirmPassword) {
+        alert("password don't match");
+        return;
+      }
+
+      try {
+        const { user } = await auth.createUserWithEmailAndPassword(email, password);
+
+        await createUserProfileDocument(user, { displayName });
+
+        this.setState({
+          displayName: '',
+          email: '',
+          password: '',
+          confirmPassword: ''  
+        })
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    
+    handleChange = event => {
+      const { value, name } = event.target; 
+      this.setState({ [name]: value });
+    }
+  
+    render() {
+      const {displayName, email, password, confirmPassword} = this.state;
+      return(
+        <div className='sign-up'>
+          <h2 className='title'>I do not have an account</h2>
+          <span>Sign up with your email and password</span>
+  
+          <form onSubmit={this.handleSubmit}>
+          <FormInput 
+              name='displayName' 
+              type='text' 
+              value={displayName}
+              handleChange={this.handleChange} 
+              required
+              label='Display Name' 
+            />
+            <FormInput 
+              name='email' 
+              type='email' 
+              value={email}
+              handleChange={this.handleChange} 
+              required
+              label='Email' 
+            />
+            
+            <FormInput 
+              name='password' 
+              type='password' 
+              value={password} 
+              handleChange={this.handleChange}
+              required
+              label='Password' 
+            />
+            <FormInput 
+              name='confirmPassword' 
+              type='password' 
+              value={confirmPassword} 
+              handleChange={this.handleChange}
+              required
+              label='Confirm Password' 
+            />
+            <div className='buttons'>
+              <CustomButton type='submit'>Sign Up</CustomButton>
+            </div>
+          </form>
+        </div>
+      ); 
+    }
+  }
+  
+  export default SignUp;
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -13,15 +13,43 @@ const config = {
   measurementId: "G-24FF87JHG1"
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  /* If user does not exist in our db */
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createAt,
+        ...additionalData
+      })
+
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+  return userRef;
+}
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
-/*provider.setCustomParameters({ prompt: 'select_account' }); */
+provider.setCustomParameters({ prompt: 'select_account' }); 
+/*
 provider.addScope('profile');
 provider.addScope('email');
+*/
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
