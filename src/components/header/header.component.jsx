51c331260c85d0bd8ceee20e541ded4bae8614b8
Marvin <44047272+marvinxu99@users.noreply.github.com>
2019-10-06 12:42:29 -0700

    added redux and implemented user reducer

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import './header.styles.scss';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
@@ -12,32 +13,35 @@ const Header = ({ currentUser }) => (
     <Link className='logo-container' to="/">
       <Logo className='logo' />
     </Link>
-    <div className='options'>
-      <Link className='option' to='/'>
-        HOME
-      </Link>
-      
-      <Link className='option' to='/shop'>
-        SHOP
-      </Link>
-      
-      <Link className='option' to='/contact'>
-        CONTACT
-      </Link>
-      
-      { /* Logic for displaying SIGN OUT or SIGN IN */
-        currentUser ?
-        <div className='option' onClick={()=>auth.signOut()}>
-          SIGN OUT
-        </div>
-        :  
-        <Link className='option' to='/signin'>
-          SIGN IN
-        </Link>
-      }
 
+    <div className='options'>
+        <Link className='option' to='/'>
+          HOME
+        </Link>
+        <Link className='option' to='/shop'>
+          SHOP
+        </Link>
+        <Link className='option' to='/contact'>
+          CONTACT
+        </Link>
+        
+        { /* Logic for displaying SIGN OUT or SIGN IN */
+          currentUser ?
+          <div className='option' onClick={()=>auth.signOut()}>
+            SIGN OUT
+          </div>
+          :  
+          <Link className='option' to='/signin'>
+              SIGN IN
+          </Link>
+        }
+        
+      </div>
     </div>
-  </div>
 );
 
-export default Header;
+const mapStateToProps = state => ({
+  currentUser: state.user.currentUser
+})
+
+export default connect(mapStateToProps)(Header);
