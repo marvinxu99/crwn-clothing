833d9d850b137d76a80ef9ee7f6c476496652fc3
Marvin <44047272+marvinxu99@users.noreply.github.com>
2019-10-06 18:59:05 -0700

    cart icon added, cart dropdown to header, added cart reducer

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -7,8 +7,11 @@ import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 import { auth } from '../../firebase/firebase.utils';
 
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
-const Header = ({ currentUser }) => (
+
+const Header = ({ currentUser, hidden }) => (
   <div className='header'>
     <Link className='logo-container' to="/">
       <Logo className='logo' />
@@ -23,8 +26,7 @@ const Header = ({ currentUser }) => (
         </Link>
         <Link className='option' to='/contact'>
           CONTACT
-        </Link>
-        
+        </Link>       
         { /* Logic for displaying SIGN OUT or SIGN IN */
           currentUser ?
           <div className='option' onClick={()=>auth.signOut()}>
@@ -35,13 +37,21 @@ const Header = ({ currentUser }) => (
               SIGN IN
           </Link>
         }
-        
+        <CartIcon />
       </div>
+      { 
+        hidden ? null : <CartDropdown />
+      }
     </div>
 );
 
+/**** */
 const mapStateToProps = state => ({
   currentUser: state.user.currentUser
+}) *****/
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden }}) => ({
+  currentUser,
+  hidden
 })
 
 export default connect(mapStateToProps)(Header);
