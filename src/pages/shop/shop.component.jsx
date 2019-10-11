fad419701f33dc928dd386be4873e2e56211ba1a
Marvin <44047272+marvinxu99@users.noreply.github.com>
2019-10-11 16:23:25 -0700

    added Switch to shoppage route

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 import CollectionPage from '../collection/collection.component';
@@ -10,9 +10,11 @@ const ShopPage = ({ match }) => {
   
   return(
     <div className='shop-page'>
+      <Switch>
         <Route exact path={`${match.path}`} component={CollectionsOverview} />
         <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
-        <Route component={PageNotFound} />    
+        <Route component={PageNotFound} /> 
+      </Switch>
     </div>
   );
 } 
