39e53655de3ccb351ec378d6e6ffe780676b5b07
marvinxu99 <marvinxu99@hotmail.com>
2023-02-20 11:57:26 -0800

    added categories componenet

--- src/App.js
+++ src/App.js
@@ -1,42 +1,13 @@
-import "./categories.styles.scss";
-
+import CategoryItem from "./components/category-item/category-item.component";
+import categories from "./components/categories/categories.components";
+import "./components/categories/categories.styles.scss";
 
 const App = () => {
-  
-  const categories = [
-    {
-      id: 1,
-      title: 'HATS',
-      imgsrc: '',
-    },
-    {
-      id: 2,
-      title: 'JACKETS',
-    },
-    {
-      id: 3,
-      title: 'SNEAKERS',
-    },
-    {
-      id: 4,
-      title: 'WOMENS',
-    },
-    {
-      id: 5,
-      title: 'MENS',
-    },
-  ]
-
+      
   return (
     <div className="categories-container">
       {categories.map((category) => (
-          <div className="category-container" key={category.id}>
-            <div className="background-image"></div>
-            <div className="category-body-container">
-              <h2>{category.title}</h2>
-              <p>Shop Now</p>
-            </div>
-          </div>
+          <CategoryItem category={category} key={category.id} />
         )
       )}
     </div>
