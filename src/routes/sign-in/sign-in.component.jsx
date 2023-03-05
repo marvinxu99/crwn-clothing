dfd3d973b25469fc06e3eb24a67c99fd10b0c797
marvinxu99 <marvinxu99@hotmail.com>
2023-03-04 20:19:30 -0800

    Sign up form

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,6 +1,8 @@
 import { useEffect } from 'react';
 import { getRedirectResult } from 'firebase/auth';
 
+import SignUpForm from '../../components/sign-up-form/sign-up-form.component';
+
 
 import { 
   auth,
@@ -41,6 +43,8 @@ const SignIn = () => {
       <button onClick={signInWithGoogleRedirect}>Sign in with Google Redirect</button>
 
       <button onClick={logGithubUser}>Sign in with Github Popup</button>
+
+      <SignUpForm />
     </div>
   )
 }
