0b9cadd4bd802e69c90ed53b5160430fb2f99d57
marvinxu99 <marvinxu99@hotmail.com>
2023-03-10 19:59:56 -0800

    added carticon and CartDropwon components

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -6,6 +6,9 @@ import { UserContext } from '../../contexts/user.context';
 
 import { signOutUser } from '../../utils/firebase/firebase.utils';
 
+import CartIcon from '../../components/cart-icon/cart-icon.component';
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
+
 import './navigation.styles.scss';
 
 
@@ -20,14 +23,14 @@ const Navigation = () => {
         </Link>
         <div className='nav-links-container'>
           <Link className='nav-link' to='/shop'>SHOP</Link>
-          {
-            currentUser ? (
+          {currentUser ? (
               <span className='nav-link' onClick={signOutUser}>SIGN OUT</span>
             ) : (
               <Link className='nav-link' to='/auth'>SIGN IN</Link>
-            )
-          }
+            )}
+          <CartIcon />
         </div>
+        <CartDropdown />
       </div>
       <Outlet />
     </Fragment>
