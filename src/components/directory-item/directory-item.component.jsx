0ef8ba0916b1149d6f0d971fbea0257009b42461
marvinxu99 <marvinxu99@hotmail.com>
2023-04-02 16:36:02 -0700

    ...

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 
 import './directory-item.styles.scss';
 
@@ -11,7 +12,7 @@ const DirectoryItem = ({category}) =>{
       />
       <div className="directory-body-container">
         <h2>{title.toUpperCase()}</h2>
-        <p>Shop Now</p>
+        <p><Link to={`shop/${title.toLowerCase()}`}>Shop Now</Link></p>
       </div>
     </div>
   )
